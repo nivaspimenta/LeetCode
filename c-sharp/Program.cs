65a0ee34b6c6c6a0827fc66ef4d47a10247ce433
Nivas <98767176+nivaspimenta@users.noreply.github.com>
2026-06-30 11:19:06 +0000

    20-ValidParentheses

--- c-sharp/Program.cs
+++ c-sharp/Program.cs
@@ -1,7 +1,2 @@
 ﻿using LeetCode.Easy;
 
-var solution = new LongestCommonPref();
-
-string[] strg = ["abc"];
-
-Console.WriteLine($"Array: [ {string.Join(", ", strg)} ] \nPrefix: {solution.LongestCommonPrefix(strg)}");
